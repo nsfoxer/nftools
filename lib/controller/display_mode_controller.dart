8c35f9c824633d3584f0215efe5fc524a9d549bc
nsfoxer <1261805497@qq.com>
2024-09-30 12:02:44 +0800

    优化theme mode ui

---
+++ lib/controller/display_mode_controller.dart
@@ -0,0 +1,37 @@
+import 'package:easy_debounce/easy_debounce.dart';
+import 'package:get/get.dart';
+import 'package:nftools/messages/display.pb.dart';
+import 'package:nftools/state/display_mode_state.dart';
+import 'package:nftools/api/display_api.dart' as $api;
+
+class DisplayModeController extends GetxController {
+  final state = DisplayModeState();
+
+  @override
+  void onReady() {
+    _init();
+    super.onReady();
+  }
+
+  void _init() async {
+    state.isLight = (await $api.getCurrentMode()).isLight;
+    var r1 = await $api.getWallpaper();
+    state.lightWallpaper = r1.lightWallpaper;
+    state.darkWallpaper = r1.darkWallpaper;
+    update();
+  }
+
+  void getMode() {
+    $api.getCurrentMode();
+  }
+  void setMode(bool light) {
+    EasyDebounce.debounce("display-mode/set-mode", const Duration(seconds: 2), () {
+      state.isLight = light;
+      update();
+      $api.setMode(DisplayMode(isLight: light)).catchError((e) {
+        state.isLight = light;
+        update();
+      });
+    });
+  }
+}
