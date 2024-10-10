4910169d18e3e073695dbe9bb36ef6487b193e38
nsfoxer <1261805497@qq.com>
2024-10-10 14:34:51 +0800

    syncfile 配置

--- lib/state/sync_file_state.dart
+++ lib/state/sync_file_state.dart
@@ -1,3 +1,9 @@
+import 'package:flutter/widgets.dart';
+
 class SyncFileState {
   List<String> files = [];
+
+  TextEditingController controller1 = TextEditingController();
+  TextEditingController controller2 = TextEditingController();
+  TextEditingController controller3 = TextEditingController();
 }
