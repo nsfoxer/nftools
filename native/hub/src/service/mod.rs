5bea6d0165857641512f39bf186e097605510c31
nsfoxer <1261805497@qq.com>
2024-10-18 18:46:02 +0800

    system info 实现

--- native/hub/src/service/mod.rs
+++ native/hub/src/service/mod.rs
@@ -2,3 +2,4 @@ pub mod display;
 pub mod service;
 pub mod syncfile;
 mod settings;
+pub mod system_info;
---
+++ native/hub/src/service/system_info.rs
@@ -0,0 +1,57 @@
+use sysinfo::{CpuRefreshKind, MemoryRefreshKind, RefreshKind, System};
+use crate::messages::common::{FloatMessage, Uint32Message};
+use crate::service::service::{LazyService, Service};
+use anyhow::Result;
+use crate::{func_end, func_notype, func_typeno};
+
+#[derive(Default)]
+pub struct SystemInfoService {
+    sys: Option<System>,
+}
+
+#[async_trait::async_trait]
+impl Service for SystemInfoService {
+    fn get_service_name(&self) -> &'static str {
+        "SystemInfo"
+    }
+
+    async fn handle(&mut self, func: &str, req_data: Vec<u8>) -> anyhow::Result<Option<Vec<u8>>> {
+        func_notype!(self, func, get_cpu, get_ram);
+        func_end!(func);
+    }
+}
+
+#[async_trait::async_trait]
+impl LazyService for SystemInfoService {
+    async fn lazy_init_self(&mut self) -> anyhow::Result<()> {
+        // 初始化监控信息
+        let res = RefreshKind::new()
+            .with_cpu(CpuRefreshKind::everything())
+            .with_memory(MemoryRefreshKind::new().with_ram());
+        let mut sys = System::new_with_specifics(res);
+        sys.refresh_all();
+        self.sys = Some(sys);
+        Ok(())
+    }
+}
+
+
+impl SystemInfoService {
+    fn get_cpu(&mut self) -> Result<FloatMessage> {
+        let mut sys = self.sys.as_ref().unwrap();
+        let value = sys.global_cpu_usage();
+        sys.refresh_cpu_usage();
+        Ok(FloatMessage {
+            value,
+        })
+    }
+
+    fn get_ram(&mut self) -> Result<FloatMessage> {
+        let mut sys = self.sys.as_ref().unwrap();
+        sys.refresh_memory();
+        let value = 1.0 - (sys.used_memory() as f32 / sys.total_memory() as f32);
+        Ok(FloatMessage {
+            value,
+        })
+    }
+}
