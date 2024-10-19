63dd0009222ea78479d89c547f72e13e4fd4749a
nsfoxer <muwuren@gmail.com>
2024-10-19 08:26:36 +0800

    后台：system info 修复

--- native/hub/src/service/system_info.rs
+++ native/hub/src/service/system_info.rs
@@ -1,8 +1,9 @@
 use sysinfo::{CpuRefreshKind, MemoryRefreshKind, RefreshKind, System};
-use crate::messages::common::{FloatMessage, Uint32Message};
+use crate::messages::common::{FloatMessage};
 use crate::service::service::{LazyService, Service};
 use anyhow::Result;
-use crate::{func_end, func_notype, func_typeno};
+use crate::{func_end, func_notype};
+use prost::Message;
 
 #[derive(Default)]
 pub struct SystemInfoService {
@@ -17,7 +18,7 @@ impl Service for SystemInfoService {
 
     async fn handle(&mut self, func: &str, req_data: Vec<u8>) -> anyhow::Result<Option<Vec<u8>>> {
         func_notype!(self, func, get_cpu, get_ram);
-        func_end!(func);
+        func_end!(func)
     }
 }
 
@@ -38,7 +39,7 @@ impl LazyService for SystemInfoService {
 
 impl SystemInfoService {
     fn get_cpu(&mut self) -> Result<FloatMessage> {
-        let mut sys = self.sys.as_ref().unwrap();
+        let mut sys = self.sys.as_mut().unwrap();
         let value = sys.global_cpu_usage();
         sys.refresh_cpu_usage();
         Ok(FloatMessage {
@@ -47,7 +48,7 @@ impl SystemInfoService {
     }
 
     fn get_ram(&mut self) -> Result<FloatMessage> {
-        let mut sys = self.sys.as_ref().unwrap();
+        let mut sys = self.sys.as_mut().unwrap();
         sys.refresh_memory();
         let value = 1.0 - (sys.used_memory() as f32 / sys.total_memory() as f32);
         Ok(FloatMessage {
