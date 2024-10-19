61580ce25f3efb1ef23f2dcf662dae7152389bab
nsfoxer <muwuren@gmail.com>
2024-10-19 23:24:37 +0800

    优化system

--- lib/state/system_info_state.dart
+++ lib/state/system_info_state.dart
@@ -1,20 +1,16 @@
 class SystemInfoState {
   Map<String, (int, int)> timeSpanCombo = {
-    "5s": (5, 1),
     "1min": (60, 1),
     "5min": (300, 1),
-    "10min": (300, 2),
-    "30min": (300,6),
-    "1h": (600, 6),
-    "5h": (900, 20),
-    "12h": (1200, 36),
-    "24h": (1200, 72),
+    "10min": (600, 1),
+    "30min": (900,2),
+    "1h": (1200, 3),
   };
   List<ValueInfo> cpuInfos = [];
   List<ValueInfo> memoryInfos = [];
 
   int _size = 60;
-  String selected = "1min";
+  String selected = "10min";
 
   void setSize(int size) {
     _size = size;
