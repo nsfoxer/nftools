e62d4daf943fb9b64f5b6ac20b712291417211f0
nsfoxer <muwuren@gmail.com>
2024-10-26 22:18:41 +0800

    前端适配协议字段改动

--- lib/api/system_info.dart
+++ lib/api/system_info.dart
@@ -9,14 +9,14 @@ const String _getCpu = "get_cpu";
 const String _getRam = "get_ram";
 
 
-Future<CpuInfoRsp> getCpu() async {
+Future<ChartInfo> getCpu() async {
   var r = await sendRequest<EmptyMessage>(_service1, _getCpu, null);
-  return CpuInfoRsp.fromBuffer(r);
+  return ChartInfo.fromBuffer(r);
 }
 
-Future<RamInfoRsp> getRam() async {
+Future<ChartInfo> getRam() async {
   var r = await sendRequest<EmptyMessage>(_service1, _getRam, null);
-  return RamInfoRsp.fromBuffer(r);
+  return ChartInfo.fromBuffer(r);
 }
 
 
