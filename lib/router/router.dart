8e1d504b8afabfe21e1781efcfd2755972108c63
nsfoxer <1261805497@qq.com>
2024-10-30 10:46:55 +0800

    前端：去除二级路由

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -5,16 +5,14 @@ import 'package:nftools/pages/system_info.dart';
 
 class MyRouterConfig {
   static List<MenuData> menuDatas = [
-    const MenuData("/", Icons.home, "主页",Text("main"), null, false),
-    const MenuData("/display", Icons.display_settings, "显示",  DisplayPage(), null, false),
-    const MenuData("/test", Icons.explore, "测试", Text("data2"), null, true),
-    const MenuData("/test/A", Icons.explore, "测试1", Text("data3"), "/test", false),
-    const MenuData("/test/B", Icons.explore, "测试2", Text("data4"), "/test", false),
-    MenuData("/systemInfo", Icons.area_chart, "系统监控", SystemInfoPage(), null, false),
+    const MenuData("/", Icons.home, "主页",Text("main")),
+    const MenuData("/display", Icons.display_settings, "显示",  DisplayPage()),
+    const MenuData("/test", Icons.explore, "测试", Text("data2")),
+    MenuData("/systemInfo", Icons.area_chart, "系统监控", SystemInfoPage()),
   ];
 
   static List<MenuData> footerDatas = [
-    const MenuData("/settings", Icons.settings, "设置", Text("setting"), null, false),
+    const MenuData("/settings", Icons.settings, "设置", Text("setting")),
   ];
 
 }
@@ -23,11 +21,9 @@ class MenuData {
   final String label;
   final String url;
   final IconData icon;
-  final String? parentUrl;
   final Widget body;
-  final bool isParent;
 
-  const MenuData(this.url, this.icon, this.label, this.body, this.parentUrl, this.isParent);
+  const MenuData(this.url, this.icon, this.label, this.body);
 }
 
 
