c68d3d9d64d64cffe2bc1d070060c09078babede
nsfoxer <1261805497@qq.com>
2024-11-19 14:51:16 +0800

    前台: 获取列表数据

--- lib/controller/sync_file_controller.dart
+++ lib/controller/sync_file_controller.dart
@@ -1,6 +1,7 @@
 import 'package:get/get.dart';
 import 'package:nftools/api/syncfile.dart' as $api;
 import 'package:nftools/state/sync_file_state.dart';
+import 'package:nftools/utils/log.dart';
 
 class SyncFileController extends GetxController {
   final state = SyncFileState();
@@ -13,10 +14,12 @@ class SyncFileController extends GetxController {
 
   // 初始化数据
   _init() async {
-    var result = await $api.listDirs();
-    state.isLoading = false;
-    state.fileList = result.files;
-    update();
+    try {
+      var result = await $api.listDirs();
+      state.fileList = result.files;
+    } finally {
+      state.isLoading = false;
+      update();
+    }
   }
-
 }
