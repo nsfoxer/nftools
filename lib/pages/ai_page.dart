06e9ac37c2a9c841f1e14c134fe306827a147f22
nsfoxer <1261805497@qq.com>
2024-12-25 17:59:08 +0800

    ai前端实现中

--- lib/pages/ai_page.dart
+++ lib/pages/ai_page.dart
@@ -1,20 +1,139 @@
+import 'dart:math';
 
 import 'package:fluent_ui/fluent_ui.dart';
-import 'package:nftools/api/ai.dart';
+import 'package:get/get.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nftools/controller/ai_controller.dart';
+import 'package:nftools/utils/log.dart';
 
 class AiPage extends StatelessWidget {
   const AiPage({super.key});
 
+  void _showKVSetting(BuildContext context) async {
+    var typography = FluentTheme.of(context).typography;
+    var color = FluentTheme.of(context).activeColor;
+    await showDialog<String>(
+        context: context,
+        builder: (context) => GetBuilder<AiController>(builder: (logic) {
+              return ContentDialog(
+                title: Text(
+                  "密钥管理",
+                  style: typography.subtitle,
+                ),
+                content: SizedBox(
+                  child: Form(
+                    key: logic.state.formKey,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        InfoLabel(
+                          label: "APP_ID",
+                          child: TextFormBox(
+                            controller: logic.state.appIdController,
+                            cursorColor: color,
+                            keyboardType: TextInputType.text,
+                            validator: (v) {
+                              return null;
+                            },
+                          ),
+                        ),
+                        InfoLabel(
+                          label: "SECRET",
+                          child: PasswordFormBox(
+                            controller: logic.state.secretController,
+                            cursorColor: color,
+                            validator: (v) {
+                              return null;
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                actions: [
+                  FilledButton(
+                      child: const Text("提交"),
+                      onPressed: () async {
+                        if (!logic.state.formKey.currentState!.validate()) {
+                          return;
+                        }
+                        if (await logic.setKV()) {
+                          info("设置成功");
+                          if (context.mounted) {
+                            context.pop();
+                          }
+                        }
+                      }),
+                  Button(
+                      child: const Text("取消"),
+                      onPressed: () {
+                        logic.state.formKey.currentState!.reset();
+                        context.pop();
+                      })
+                ],
+              );
+            }));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Button(child: Text("ai"), onPressed: () {
-        final stream = question("好饿");
-        stream.listen( (rsp) {
-          debugPrint(rsp.content);
-        });
+    return ScaffoldPage(
+      header: PageHeader(
+        title: const Text("BaiduAI"),
+        commandBar: GetBuilder<AiController>(builder: (logic) {
+          return CommandBar(
+            mainAxisAlignment: MainAxisAlignment.end,
+            overflowItemAlignment: MainAxisAlignment.end,
+            primaryItems: [
+              CommandBarButton(
+                  icon: const Icon(FluentIcons.account_management),
+                  label: const Text("账户管理"),
+                  onPressed: () {
+                    _showKVSetting(context);
+                  }),
+              CommandBarButton(
+                  icon: const Icon(FluentIcons.refresh),
+                  label: const Text("刷新"),
+                  onPressed: () {}),
+            ],
+          );
+        }),
+      ),
+      content: GetBuilder<AiController>(builder: (logic) {
+        return Column(
+          children: [
+            Expanded(
+              flex: 9,
+              child: TextBox(
+                readOnly: true,
+                controller: logic.state.displayController,
+              ),
+            ),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  flex: 9,
+                  child: TextBox(
+                    minLines: null,
+                    maxLines: null,
+                    controller: logic.state.questController,
+                  ),
+                ),
+                Expanded(
+                    flex: 1,
+                    child: Button(
+                        child: const Text("提问"),
+                        onPressed: () {
+                          logic.quest();
+                        }))
+              ],
+            )
+          ],
+        );
       }),
     );
   }
-
 }
---
+++ lib/state/ai_state.dart
@@ -0,0 +1,23 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class AiState {
+  // 展示结果
+  TextEditingController displayController = TextEditingController();
+  // 输入问题
+  TextEditingController questController = TextEditingController();
+
+  // 账户信息输入表单
+  GlobalKey<FormState> formKey = GlobalKey();
+  // user控制器
+  TextEditingController appIdController = TextEditingController();
+  // passwd控制器
+  TextEditingController secretController = TextEditingController();
+
+  void dispose() {
+    debugPrint("dispose start");
+    appIdController.dispose();
+    secretController.dispose();
+    questController.dispose();
+    displayController.dispose();
+  }
+}
