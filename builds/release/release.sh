208173a6d91fa5539f50832341cbb58ef3724e09
nsfoxer <1261805497@qq.com>
2025-01-20 17:26:53 +0800

    前台：更新服务地址

--- builds/release/release.sh
+++ builds/release/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 1. 上传发布文件
-.\ossutil64.exe cp server.json oss://nsfoxer-oss/nftools/server.json
+..\..\..\..\bin\ossutil64.exe cp .\builds\release\server.json oss://nsfoxer-oss/nftools/server.json
 
 # todo 2. 运行编译
 # todo 3. 发布
