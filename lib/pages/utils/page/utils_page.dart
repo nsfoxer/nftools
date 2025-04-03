61960c0519b4d39b4fb2c98371be5000879cae39
nsfoxer <1261805497@qq.com>
2025-04-03 12:08:42 +0800

    前端 - 样式修改

--- lib/pages/utils/page/utils_page.dart
+++ lib/pages/utils/page/utils_page.dart
@@ -1,4 +1,5 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:nftools/utils/nf_widgets.dart';
 
 class UtilsPage extends StatelessWidget {
   const UtilsPage({super.key});
@@ -7,17 +8,18 @@ class UtilsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     Typography typography = FluentTheme.of(context).typography;
     return ScaffoldPage(
-      header: PageHeader(
-        title: Text('Utils', style: typography.title),
-      ),
-      content: const Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text("文本对比")
-        ]
-      )
-    );
+        header: PageHeader(
+          title: Text('Utils', style: typography.title),
+        ),
+        content: const Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              NFCardContent(
+                  child: SizedBox(
+                      width: 100,
+                      height: 80,
+                      child: Center(child: Text("文本对比"))))
+            ]));
   }
-
 }
