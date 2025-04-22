d4276d9e731259d0efd36a2cef3038d1deb0edf1
nsfoxer <1261805497@qq.com>
2025-04-22 09:50:30 +0800

    前端 - 优化utils布局

--- lib/pages/utils/page/utils_page.dart
+++ lib/pages/utils/page/utils_page.dart
@@ -1,5 +1,9 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nftools/common/style.dart';
 import 'package:nftools/utils/nf_widgets.dart';
+
+import '../../../common/constants.dart';
 
 class UtilsPage extends StatelessWidget {
   const UtilsPage({super.key});
@@ -11,15 +15,31 @@ class UtilsPage extends StatelessWidget {
         header: PageHeader(
           title: Text('Utils', style: typography.title),
         ),
-        content: const Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              NFCardContent(
-                  child: SizedBox(
-                      width: 100,
-                      height: 80,
-                      child: Center(child: Text("文本对比"))))
-            ]));
+        content: Container(
+            margin: const EdgeInsets.symmetric(horizontal: NFLayout.v0),
+            child: const Wrap(
+                spacing: NFLayout.v1,
+                runSpacing: NFLayout.v1,
+                children: [
+                  _UtilsPage(url: "/utils/diffText", child: Text("文本对比")),
+                ])));
+  }
+}
+
+class _UtilsPage extends StatelessWidget {
+  const _UtilsPage({super.key, required this.url, required this.child});
+
+  final String url;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return Button(
+        child: SizedBox(
+          width: 100,
+          height: 80,
+          child: Center(child: child),
+        ),
+        onPressed: () => context.go(url));
   }
 }
