7461340678f4296bd21b32d9d5b27738490912ad
nsfoxer <1261805497@qq.com>
2025-05-16 16:33:12 +0800

    前端 - 日志优化

--- lib/utils/log.dart
+++ lib/utils/log.dart
@@ -1,40 +1,55 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:logger/logger.dart';
 import 'package:nftools/router/router.dart';
 
-const Map<InfoBarSeverity, String> _infoNameMap = {
-  InfoBarSeverity.info: "通知",
-  InfoBarSeverity.success: "成功",
-  InfoBarSeverity.warning: "警告",
-  InfoBarSeverity.error: "错误",
+final _logger =Logger(printer: HybridPrinter(SimplePrinter(), error: PrettyPrinter(), fatal: PrettyPrinter()));
+
+const Map<Level, (InfoBarSeverity, String)> _infoMap = {
+  Level.info: (InfoBarSeverity.info, "通知"),
+  Level.warning: (InfoBarSeverity.warning, "警告"),
+  Level.error: (InfoBarSeverity.error, "错误"),
+  Level.fatal: (InfoBarSeverity.error, "错误"),
 };
 
 /// info log
+void debug(String message) {
+  _print(message, Level.debug);
+}
+/// info log
 void info(String message) {
-  _print(message, InfoBarSeverity.info);
+  _print(message, Level.info);
 }
 
 /// warn log
 void warn(String message) {
-  _print(message, InfoBarSeverity.warning);
+  _print(message, Level.warning);
 }
 
 /// error log
 void error(String message) {
-  _print(message, InfoBarSeverity.error);
+  _print(message, Level.error);
 }
 
-void _print(String message, InfoBarSeverity serverity) async {
-  debugPrint("${serverity.name}: $message");
+/// error log
+void fatal(String message) {
+  _print(message, Level.fatal);
+}
+
+void _print(String message, Level level) async {
+  _logger.log(level, message);
+
   final context = MyRouterConfig.themeContext;
-  if (context == null) {
+  final value = _infoMap[level];
+  if (context == null || value == null) {
     return;
   }
+
   await displayInfoBar(context, duration: const Duration(seconds: 3),
       builder: (context, close) {
     return InfoBar(
       content: Text(message),
-      severity: serverity,
-      title: Text(_infoNameMap[serverity] ?? ""),
+      severity: value.$1,
+      title: Text(value.$2),
       isLong: message.length > 30,
       isIconVisible: true,
     );
