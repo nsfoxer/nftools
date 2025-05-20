784719548e9452e365978f600c968866ce751347
nsfoxer <1261805497@qq.com>
2025-05-20 16:23:45 +0800

    实现二维码识别及转换

--- lib/pages/utils/controller/qr_controller.dart
+++ lib/pages/utils/controller/qr_controller.dart
@@ -1,14 +1,18 @@
 
+import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:easy_debounce/easy_debounce.dart';
-import 'package:fluent_ui/fluent_ui.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/src/widgets/framework.dart';
 import 'package:get/get_state_manager/src/simple/get_controllers.dart';
 import 'package:nftools/pages/utils/state/qr_encode_state.dart';
+import 'package:nftools/src/bindings/signals/signals.dart';
 import 'package:pasteboard/pasteboard.dart';
 
 import '../../../common/constants.dart';
 import '../../../api/utils.dart' as $api;
+import '../../../utils/log.dart';
 
 class QrController extends GetxController {
   QrEncodeState state = QrEncodeState();
@@ -19,21 +23,12 @@ class QrController extends GetxController {
   void onInit() {
     super.onInit();
     _init();
-    state.codeLineEditingController.addListener(() {
-      if (_lastText == state.codeLineEditingController.text) {
-        return;
-      }
-      EasyDebounce.debounce(PageWidgetNameConstant.qrCodeText, const Duration(milliseconds: 1000), () {
-        _lastText = state.codeLineEditingController.text;
-        _generateText(state.codeLineEditingController.text);
-      });
-    });
   }
 
   void _init() async {
     // 初始化监听器
     state.codeLineEditingController.addListener(() {
-      if (_lastText == state.codeLineEditingController.text) {
+      if (!state.isData2Qr || _lastText == state.codeLineEditingController.text) {
         return;
       }
       EasyDebounce.debounce(PageWidgetNameConstant.qrCodeText, const Duration(milliseconds: 1000), () {
@@ -47,9 +42,12 @@ class QrController extends GetxController {
     if (text != null && text.isNotEmpty) {
       state.codeLineEditingController.text = text;
     }
+
+    state.imageFocus.requestFocus();
   }
 
   void _generateText(String text) async {
+    state.filePath = null;
     // 无数据
     if (text.isEmpty) {
       state.imageData = Uint8List(0);
@@ -69,6 +67,7 @@ class QrController extends GetxController {
 
   void generateFile(String path) async {
     state.isLoading = true;
+    state.filePath = path;
     update();
     try {
       state.imageData = await $api.genFileQrCode(path);
@@ -82,5 +81,106 @@ class QrController extends GetxController {
   void onClose() {
     state.codeLineEditingController.dispose();
     super.onClose();
+  }
+
+  // 解码粘贴板的图片
+  void decodePasteboardImage() async {
+    final image = await Pasteboard.image;
+    if (image == null || image.isEmpty) {
+      warn("未获取到剪贴板中图像");
+      return;
+    }
+    _detectImage(image);
+  }
+
+  void _detectImage(Uint8List imageData) async {
+    state.qRData = null;
+    state.imageDataForDecode = imageData;
+    update();
+    final data = await $api.detectQrCode(imageData);
+    state.qRData = data;
+    update();
+  }
+
+  // 切换类型 文本转图 图转文本
+  void switchType() {
+    state.isData2Qr = ! state.isData2Qr;
+    reset();
+  }
+
+  // 解析图片
+  void handleQr(QrCodeDataMsg e) {
+    // 1. 清空数据
+    state.codeLineEditingController.text = "";
+
+    // 2. 尝试转换数据为字符串
+    state.fileData = e.data;
+    debug("${e.data.length}");
+    try {
+      // 转换为字符串
+      final text = String.fromCharCodes(e.data);
+      state.codeLineEditingController.text = text;
+    } catch (ignored) {
+      // 转换为文件
+    }
+    update();
+  }
+
+  void handleFile() async {
+    // 文件转换为二维码
+    if (state.isData2Qr) {
+      final path = await _getLocalFile(FileType.any);
+      if (path == null) {
+        return;
+      }
+      generateFile(path);
+      return;
+    }
+
+    // 二维码保存至文件
+    final path = await _getLocalDirectory();
+    if (path == null) {
+      return;
+    }
+    // 按当前时间戳创建临时文件
+    final filePath = "$path/二维码识别-${DateTime.now().millisecondsSinceEpoch}.txt";
+    final file = File(filePath);
+    await file.writeAsBytes(state.fileData);
+    state.filePath = filePath;
+    info("保存文件至: ${file.path}");
+    update();
+  }
+
+  // 获取本地文件
+  Future<String?> _getLocalFile(FileType fileType) async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(type: fileType);
+    return result?.files.single.path;
+  }
+
+  // 获取本地目录
+  Future<String?> _getLocalDirectory() async {
+    return await FilePicker.platform.getDirectoryPath();
+  }
+
+  // 重置
+  void reset() {
+    state.imageData = Uint8List(0);
+    state.codeLineEditingController.text = "";
+    _lastText = "";
+    state.fileData = [];
+    state.imageDataForDecode = Uint8List(0);
+    state.filePath = null;
+    update();
+  }
+
+  // 读取图片
+  void readImage() async {
+    final path = await _getLocalFile(FileType.any);
+    if (path == null) {
+      return;
+    }
+    final file = File(path);
+    final bytes = await file.readAsBytes();
+    _detectImage(bytes);
   }
 }
--- lib/pages/utils/page/qr_page.dart
+++ lib/pages/utils/page/qr_page.dart
@@ -1,8 +1,12 @@
-import 'package:file_picker/file_picker.dart';
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:nftools/common/style.dart';
 import 'package:nftools/pages/utils/controller/qr_controller.dart';
 import 'package:nftools/utils/nf_widgets.dart';
+
+import '../../../src/bindings/bindings.dart';
+import '../../../utils/log.dart';
 
 class QrPage extends StatelessWidget {
   const QrPage({super.key});
@@ -10,7 +14,36 @@ class QrPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScaffoldPage.withPadding(
-      header: const PageHeader(title: Text("二维码工具")),
+      header: PageHeader(
+          title: const Text("二维码工具"),
+          commandBar: GetBuilder<QrController>(
+              builder: (logic) => CommandBar(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    primaryItems: [
+                      CommandBarButton(
+                          icon: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              const Icon(FluentIcons.plain_text),
+                              NFLayout.hlineh2,
+                              Icon(logic.state.isData2Qr
+                                  ? FluentIcons.double_chevron_right8
+                                  : FluentIcons.double_chevron_left8),
+                              NFLayout.hlineh2,
+                              const Icon(FluentIcons.q_r_code)
+                            ],
+                          ),
+                          label:
+                              Text(logic.state.isData2Qr ? "二维码生成" : "二维码识别"),
+                          onPressed: () {
+                            logic.switchType();
+                          }),
+                      CommandBarButton(
+                          icon: const Icon(FluentIcons.reset),
+                          label: const Text("重置"),
+                          onPressed: () => logic.reset()),
+                    ],
+                  ))),
       content: GetBuilder<QrController>(
           builder: (logic) => Row(
                 children: [
@@ -22,6 +55,7 @@ class QrPage extends StatelessWidget {
                             flex: 1,
                             child: NFCodeEditor(
                                 wordWrap: true,
+                                readOnly: !logic.state.isData2Qr,
                                 hint: "请输入要转换的文本",
                                 controller:
                                     logic.state.codeLineEditingController),
@@ -30,43 +64,165 @@ class QrPage extends StatelessWidget {
                               flex: 1,
                               child: NFCardContent(
                                   child: GestureDetector(
-                                onTap: () async{
-                                  final path = await _getLocalFile();
-                                  if (path == null) {
-                                    return;
-                                  }
-                                  logic.generateFile(path);
-                                },
-                                child: const MouseRegion(
+                                onTap: () => logic.handleFile(),
+                                child: MouseRegion(
                                   cursor: SystemMouseCursors.click,
-                                  child: Center(
-                                    child: Text("选取要转换的文件"),
-                                  ),
+                                  child: Center(child: () {
+                                    final path = logic.state.filePath != null
+                                        ? "\n(${logic.state.filePath})"
+                                        : "";
+                                    if (logic.state.isData2Qr) {
+                                      return Text("选取要转换的文件$path",
+                                          textAlign: TextAlign.center);
+                                    }
+                                    if (logic.state.fileData.isEmpty) {
+                                      return const Text("暂未解析出数据");
+                                    }
+                                    return Text("点击保存解析出的数据$path",
+                                        textAlign: TextAlign.center);
+                                  }()),
                                 ),
                               )))
                         ],
                       )),
-                  const Divider(
-                    direction: Axis.vertical,
-                  ),
+                  Icon(logic.state.isData2Qr
+                      ? FluentIcons.double_chevron_right8
+                      : FluentIcons.double_chevron_left8),
+                  NFLayout.hlineh2,
                   Expanded(
                       flex: 1,
-                      child: NFLoadingWidgets(
-                          loading: logic.state.isLoading,
-                          child: Center(
-                              child: logic.state.imageData.isEmpty
-                                  ? null
-                                  : Image(
-                                      image: MemoryImage(logic.state.imageData),
-                                    )))),
+                      child: () {
+                        // 是展示图片结果
+                        if (logic.state.isData2Qr) {
+                          return NFLoadingWidgets(
+                              loading: logic.state.isLoading,
+                              child: Center(
+                                  child: logic.state.imageData.isEmpty
+                                      ? const Text("二维码")
+                                      : Image(
+                                          image: MemoryImage(
+                                              logic.state.imageData),
+                                        )));
+                        }
+                        // 图片识别
+                        if (logic.state.imageDataForDecode.isEmpty) {
+                          return NFCardContent(
+                              child: Shortcuts(
+                                  shortcuts: {
+                                LogicalKeySet(LogicalKeyboardKey.control,
+                                        LogicalKeyboardKey.keyV):
+                                    _SubmitIntent(logic)
+                              },
+                                  child: Actions(
+                                      actions: {
+                                        _SubmitIntent:
+                                            CallbackAction<_SubmitIntent>(
+                                                onInvoke:
+                                                    (_SubmitIntent intent) {
+                                          intent.logic.decodePasteboardImage();
+                                          return null;
+                                        })
+                                      },
+                                      child: GestureDetector(
+                                          onTap: () async {
+                                            logic.readImage();
+                                          },
+                                          child: Focus(
+                                            focusNode: logic.state.imageFocus,
+                                            autofocus: true,
+                                            child: MouseRegion(
+                                              onEnter: (event) {
+                                                logic.state.imageFocus
+                                                    .requestFocus();
+                                              },
+                                              cursor: SystemMouseCursors.click,
+                                              child: const Center(
+                                                child: Text("选择图片或ctrl+v粘贴"),
+                                              ),
+                                            ),
+                                          )))));
+                        }
+
+                        return LayoutBuilder(builder:
+                            (BuildContext context, BoxConstraints constraints) {
+                          List<Positioned> list = _buildQrCodeData(
+                              logic.state.qRData,
+                              constraints.maxWidth,
+                              constraints.maxHeight,
+                              context,
+                              logic);
+                          return Stack(children: [
+                            Container(
+                                color: Colors.blue,
+                                child: Image(
+                                    fit: BoxFit.contain,
+                                    image: MemoryImage(
+                                        logic.state.imageDataForDecode))),
+                            ...list,
+                          ]);
+                        });
+                      }()),
                 ],
               )),
     );
   }
 
-  // 获取本地文件
-  Future<String?> _getLocalFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles();
-    return result?.files.single.path;
+  List<Positioned> _buildQrCodeData(
+      QrCodeDataMsgList? qrCodeData,
+      double maxWidth,
+      double maxHeight,
+      BuildContext context,
+      QrController logic) {
+    // 1. 为空时直接返回
+    if (qrCodeData == null) {
+      return [];
+    }
+
+    // 2. 计算缩放比例 选择较小的缩放比例
+    final wRatio = maxWidth / qrCodeData.imageWidth;
+    final hRatio = maxHeight / qrCodeData.imageHeight;
+    var ratio = wRatio < hRatio ? wRatio : hRatio;
+    debug("${qrCodeData.imageWidth} ${maxWidth}  ${qrCodeData.imageHeight} $maxHeight $wRatio $hRatio");
+    if (maxWidth > qrCodeData.imageWidth && maxHeight > qrCodeData.imageHeight) {
+      ratio = 1;
+    }
+
+    // 3. 构建
+    final color =
+        FluentTheme.of(context).accentColor.normal.withValues(alpha: 0.7);
+    final typography = FluentTheme.of(context).typography;
+    return qrCodeData.value.map((e) {
+      final height = (e.bl.item2 - e.tl.item2) * ratio;
+      final width = (e.tr.item1 - e.tl.item1) * ratio;
+      return Positioned(
+        left: (e.tl.item1 + e.tr.item1) / 2 * ratio - (height / 2),
+        top: (e.tl.item2 + e.bl.item2) / 2 * ratio - (width / 2),
+        width: width,
+        height: height,
+        child: Container(
+          color: color,
+          child: Center(
+            child: Tooltip(
+              message: "识别",
+              child: IconButton(
+                  icon: Icon(
+                    FluentIcons.q_r_code,
+                    size: typography.bodyLarge?.fontSize,
+                  ),
+                  onPressed: () {
+                    logic.handleQr(e);
+                  }),
+            ),
+          ),
+        ),
+      );
+    }).toList();
   }
+}
+
+// 快捷键intent
+class _SubmitIntent extends Intent {
+  final QrController logic;
+
+  const _SubmitIntent(this.logic);
 }
