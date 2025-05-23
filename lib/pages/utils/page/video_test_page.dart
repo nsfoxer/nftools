88bba7e4e2ebd33313ba00ce34c6d1c7eb8e1d8d
nsfoxer <1261805497@qq.com>
2025-05-23 15:32:11 +0800

    前端 - 版本更新 -- re-editor在flutter v3.32.0有重大缺陷，目前该项目不可用，需等作者修复

--- lib/pages/utils/page/video_test_page.dart
+++ lib/pages/utils/page/video_test_page.dart
@@ -1,33 +1,32 @@
 
-import 'package:fluent_ui/fluent_ui.dart';
-import 'package:get/get_state_manager/src/simple/get_state.dart';
-import 'package:media_kit_video/media_kit_video.dart';
-import 'package:nftools/pages/utils/controller/video_test_controller.dart';
-
-class VideoTestPage extends StatelessWidget {
-  const VideoTestPage({super.key});
-
-
-  @override
-  Widget build(BuildContext context) {
-    // _player.open(
-    //   Media("file://C:/Users/12618/Videos/untitled.mp4")
-    // );
-    return ScaffoldPage.withPadding(
-      header: const PageHeader(title: Text("video test")),
-      content: Center(
-        child: GetBuilder<VideoTestController>(
-          builder: (logic) {
-            if (logic.controller == null) {
-              return const Text("loading");
-            }
-            return Video(
-              controller: logic.controller!,
-            );
-          },
-        )
-      ),
-    );
-  }
-
-}
+// import 'package:fluent_ui/fluent_ui.dart';
+// import 'package:get/get_state_manager/src/simple/get_state.dart';
+// import 'package:media_kit_video/media_kit_video.dart';
+// import 'package:nftools/pages/utils/controller/video_test_controller.dart';
+//
+// class VideoTestPage extends StatelessWidget {
+//   const VideoTestPage({super.key});
+//
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     // _player.open(
+//     //   Media("file://C:/Users/12618/Videos/untitled.mp4")
+//     // );
+//     return ScaffoldPage.withPadding(
+//       header: const PageHeader(title: Text("video test")),
+//       content: Center(
+//         child: GetBuilder<VideoTestController>(
+//           builder: (logic) {
+//             if (logic.controller == null) {
+//               return const Text("loading");
+//             }
+//             return Video(
+//               controller: logic.controller!,
+//             );
+//           },
+//         )
+//       ),
+//     );
+//   }
+// }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,20 +6,13 @@
 
 #include "generated_plugin_registrant.h"
 
-#include <media_kit_libs_windows_video/media_kit_libs_windows_video_plugin_c_api.h>
-#include <media_kit_video/media_kit_video_plugin_c_api.h>
 #include <pasteboard/pasteboard_plugin.h>
 #include <screen_retriever_windows/screen_retriever_windows_plugin_c_api.h>
 #include <tray_manager/tray_manager_plugin.h>
 #include <url_launcher_windows/url_launcher_windows.h>
-#include <volume_controller/volume_controller_plugin_c_api.h>
 #include <window_manager/window_manager_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
-  MediaKitLibsWindowsVideoPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("MediaKitLibsWindowsVideoPluginCApi"));
-  MediaKitVideoPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("MediaKitVideoPluginCApi"));
   PasteboardPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("PasteboardPlugin"));
   ScreenRetrieverWindowsPluginCApiRegisterWithRegistrar(
@@ -28,8 +21,6 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("TrayManagerPlugin"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
-  VolumeControllerPluginCApiRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("VolumeControllerPluginCApi"));
   WindowManagerPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("WindowManagerPlugin"));
 }
