e142b4eddf77f2e6bcef587b6878d775f9bb32a1
nsfoxer <1261805497@qq.com>
2025-06-27 17:24:57 +0800

    前端: 图形分割...

--- lib/pages/image/page/image_split_page.dart
+++ lib/pages/image/page/image_split_page.dart
@@ -1,6 +1,10 @@
 import 'package:fluent_ui/fluent_ui.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:keymap/keymap.dart';
+import 'package:nftools/utils/nf_widgets.dart';
 
+import '../../../common/style.dart';
 import '../controller/image_split_controller.dart';
 
 class ImageSplitPage extends StatelessWidget {
@@ -9,26 +13,53 @@ class ImageSplitPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScaffoldPage.withPadding(
-      header: PageHeader(
-          title: const Text("前景分割"),
-          commandBar: GetBuilder<ImageSplitController>(
-            builder: (logic) => CommandBar(
-                mainAxisAlignment: MainAxisAlignment.end,
-                primaryItems: [
-                  CommandBarButton(
-                    icon: Icon(FluentIcons.next),
-                    label: Text("下一步"),
-                    onPressed: () {},
-                  ),
-                  CommandBarButton(
-                      icon: Icon(FluentIcons.clear),
-                      label: Text("重置"),
-                      onPressed: () {}),
-                ]),
-          )),
-      content: Center(
-        child: Text("ImageSplitPage"),
-      ),
-    );
+        header: PageHeader(
+            title: const Text("前景分割"),
+            commandBar: GetBuilder<ImageSplitController>(
+              builder: (logic) => CommandBar(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  primaryItems: [
+                    CommandBarButton(
+                      icon: Icon(FluentIcons.next),
+                      label: Text("下一步"),
+                      onPressed: () {},
+                    ),
+                    CommandBarButton(
+                        icon: Icon(FluentIcons.clear),
+                        label: Text("重置"),
+                        onPressed: () {}),
+                  ]),
+            )),
+        content: GetBuilder<ImageSplitController>(builder: (logic) {
+          return KeyboardWidget(
+            bindings: [
+              KeyAction(LogicalKeyboardKey.keyV, "粘贴图像", () {},
+                  isControlPressed: true),
+            ],
+            child: logic.state.originalImage == null
+                ? GestureDetector(
+                    onTapUp: (details) {
+                      logic.setFileImg();
+                    },
+                    child: NFCardContent(
+                        child: Center(child: Text("选择图片或ctrl+v粘贴"))),
+                  )
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                        CommandBar(primaryItems: [
+                          // 操作
+                        ]),
+                        NFLayout.vlineh0,
+                        Expanded(
+                            child: NFLoadingWidgets(
+                          loading: logic.state.isLoading,
+                          child: NFImagePainterPage(
+                              controller: logic.state.controller),
+                        ))
+                      ]),
+          );
+        }));
   }
 }
