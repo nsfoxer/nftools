1c455f441132ded1ce96a8148a546168216711de
nsfoxer <1261805497@qq.com>
2025-08-13 17:25:23 +0800

    后台: 补充tar_pdf

--- native/hub/src/messages/tar_pdf.rs
+++ native/hub/src/messages/tar_pdf.rs
@@ -7,3 +7,15 @@ pub struct TarPdfMsg {
     pub(crate) sum: u32,
     pub(crate) current_file: String,
 }
+
+#[derive(Debug, Serialize, Deserialize, SignalPiece)]
+pub struct TarPdfResultsMsg {
+    pub datas: Vec<TarPdfResultMsg>
+}
+
+#[derive(Debug, Serialize, Deserialize, SignalPiece)]
+pub struct TarPdfResultMsg {
+    pub file_name: String,
+    pub title: String,
+    pub error_msg: String,
+}
