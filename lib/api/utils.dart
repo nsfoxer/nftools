56d8ef94f65572baa20dc128b507f0e7f79e2aad
nsfoxer <1261805497@qq.com>
2025-08-21 10:34:41 +0800

    前端: 添加数据存储和获取API

--- lib/api/utils.dart
+++ lib/api/utils.dart
@@ -1,4 +1,3 @@
-
 import 'dart:typed_data';
 
 import '../common/constants.dart';
@@ -15,11 +14,14 @@ const String _genFileQrCode = "gen_file_qr_code";
 const String _detectQrCode = "detect_qr_code";
 const String _detectFileQrCode = "detect_file_qr_code";
 const String _spiltImage = "split_img";
+const String _setData = "set_data";
+const String _getData = "get_data";
 
 // 压缩本地图片
-Future<CompressLocalPicRspMsg> compressLocalFile(String localFile, int width, int height) async {
-  var data = await sendRequest<CompressLocalPicMsg>
-    (_service, _compressLocalPic, CompressLocalPicMsg(localFile: localFile, width: width, height: height));
+Future<CompressLocalPicRspMsg> compressLocalFile(
+    String localFile, int width, int height) async {
+  var data = await sendRequest<CompressLocalPicMsg>(_service, _compressLocalPic,
+      CompressLocalPicMsg(localFile: localFile, width: width, height: height));
   return CompressLocalPicRspMsg.bincodeDeserialize(data);
 }
 
@@ -30,41 +32,51 @@ void notify(String msg) {
 
 // 网络状态
 Future<bool> networkStatus() async {
-  var data = await sendRequest<EmptyMsg>
-    (_service, _networkStatus, null);
+  var data = await sendRequest<EmptyMsg>(_service, _networkStatus, null);
   return BoolMsg.bincodeDeserialize(data).value;
 }
 
 // 生成文本二维码
 Future<Uint8List> genTextQrCode(String text) async {
-  final data = await sendRequest<StringMsg>
-    (_service, _genTextQrCode, StringMsg(value: text));
+  final data = await sendRequest<StringMsg>(
+      _service, _genTextQrCode, StringMsg(value: text));
   return Uint8List.fromList(DataMsg.bincodeDeserialize(data).value);
 }
 
 // 生成文件二维码
 Future<Uint8List> genFileQrCode(String text) async {
-  final data = await sendRequest<StringMsg>
-    (_service, _genFileQrCode, StringMsg(value: text));
+  final data = await sendRequest<StringMsg>(
+      _service, _genFileQrCode, StringMsg(value: text));
   return Uint8List.fromList(DataMsg.bincodeDeserialize(data).value);
 }
 
 // 探测图片二维码
 Future<QrCodeDataMsgList> detectQrCode(Uint8List data) async {
-  final result = await sendRequest<DataMsg>
-    (_service, _detectQrCode, DataMsg(value: data.toList()));
+  final result = await sendRequest<DataMsg>(
+      _service, _detectQrCode, DataMsg(value: data.toList()));
   return QrCodeDataMsgList.bincodeDeserialize(result);
 }
+
 // 探测文件二维码
 Future<QrCodeDataMsgList> detectFileQrCode(String path) async {
-  final result = await sendRequest<StringMsg>
-    (_service, _detectFileQrCode, StringMsg(value: path));
+  final result = await sendRequest<StringMsg>(
+      _service, _detectFileQrCode, StringMsg(value: path));
   return QrCodeDataMsgList.bincodeDeserialize(result);
 }
 
 /// 图片切割
 Future<Uint8List> splitImage(SplitImageMsg imgMsg) async {
-  final result = await sendRequest
-    (_service, _spiltImage, imgMsg);
+  final result = await sendRequest(_service, _spiltImage, imgMsg);
   return Uint8List.fromList(DataMsg.bincodeDeserialize(result).value);
+}
+
+/// 存储数据
+Future<void> setData(String key, String value) async {
+  await sendRequest(_service, _setData, PairStringMsg(key: key, value: value));
+}
+
+/// 获取数据
+Future<String> getData(String key) async {
+  final result = await sendRequest(_service, _getData, StringMsg(value: key));
+  return StringMsg.bincodeDeserialize(result).value;
 }
