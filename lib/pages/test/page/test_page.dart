4fbce5aaa53b367075532764c36bf815c769525d
nsfoxer <1261805497@qq.com>
2025-08-28 10:58:28 +0800

    前端:补充图像切割粘贴板缺失

--- lib/pages/test/page/test_page.dart
+++ lib/pages/test/page/test_page.dart
@@ -1,66 +1,16 @@
 import 'dart:io';
+import 'dart:math' as math;
 
 import 'package:fluent_ui/fluent_ui.dart';
 
 import '../../../utils/nf_widgets.dart';
 
 class TestPage extends StatelessWidget {
-  final NFImagePainterController controller =
-      NFImagePainterController(type:DrawType.rect, width:10.0, color:Colors.red);
 
-  TestPage({super.key});
+  const TestPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final imageProvider = Image.file(
-      File(r"C:\Users\12618\Pictures\wallpaper\【哲风壁纸】CP-动物背影.png"),
-      fit: BoxFit.contain,
-    ).image;
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Expanded(
-            child: Container(
-          color: Colors.white,
-          child: NFImagePainterPage(controller: controller),
-        )),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Button(
-                child: Text("set image"),
-                onPressed: () {
-                  controller.setImageProvider(imageProvider);
-                }),
-            Button(
-                child: Text("rect"),
-                onPressed: () {
-                  controller.changeDrawType(DrawType.rect, 1, Colors.red);
-                }),
-            Button(
-                child: Text("erase"),
-                onPressed: () {
-                  controller.changeDrawType(DrawType.erase, 1, Colors.blue);
-                }),
-            Button(
-                child: Text("path"),
-                onPressed: () {
-                  controller.changeDrawType(DrawType.path, 1, Colors.green);
-                }),
-            Button(
-              child: Text("reset"),
-              onPressed: () {
-                controller.reset();
-              },
-            ),
-            Button(
-                child: Text("save"),
-                onPressed: () {
-                  controller.saveCanvas();
-                })
-          ],
-        ),
-      ],
-    );
+    return Center();
   }
 }
